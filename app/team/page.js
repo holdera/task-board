80d538fa94ceff415e60e95aac7594e9c3170d54
holdera <holdera2@hotmail.com>
2024-08-27 20:28:23 -0400

    fix: adds team page

---
+++ app/team/page.js
@@ -0,0 +1,12 @@
+import Container from '@/components/UI/Container';
+import { h1Styles } from '../../styles/styles';
+
+export default function TeamPage() {
+	return (
+		<section>
+			<Container>
+				<h1 className={h1Styles}>Team</h1>
+			</Container>
+		</section>
+	);
+}
